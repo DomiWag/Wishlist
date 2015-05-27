06a34b5f76018c5bd7df11862faf9ef1b020e682
Dominik Wagner <DomiWag@users.noreply.github.com>
2015-05-27 14:59:20 +0200

    Add model for tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527115309) do
+ActiveRecord::Schema.define(version: 20150527124432) do
 
   create_table "gifts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20150527115309) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
