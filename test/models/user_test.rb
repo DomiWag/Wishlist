5a55ec616ce7f012297f6e8dfe779d949be21146
Dominik Wagner <DomiWag@users.noreply.github.com>
2015-05-27 15:13:41 +0200

    Add test for unique email on user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "email must be unique" do
+    assert_raise ActiveRecord::RecordInvalid do
+      User.create! email: users(:one).email
+    end
+  end
 end
