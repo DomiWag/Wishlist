4d3450eef70cff8bd94a3322d6a5fc65290a65e7
Dominik Wagner <DomiWag@users.noreply.github.com>
2015-05-27 14:59:49 +0200

    Add test for unique tag name

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -4,4 +4,10 @@ class TagTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "tag name must be unique" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Tag.create! name: tags(:one).name
+    end
+  end
 end
