b2de3e943b157c5decc7d72ffe2f95b0247edb0f
Dominik Wagner <DomiWag@users.noreply.github.com>
2015-05-27 18:21:39 +0200

    Update views in gifts, add seeds and set a temporary root

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -1,2 +1,11 @@
 module GiftsHelper
+  SHORT_DESCRIPTION_LENGTH = 100
+
+  def shorten(description)
+    if description.length > SHORT_DESCRIPTION_LENGTH then
+      description.first(SHORT_DESCRIPTION_LENGTH) + '...'
+    else
+      description
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# ruby encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create(
+    [
+        {
+            email: 'user@example.com',
+            password: 'test1234'
+        }
+    ]
+)
+
+gifts = Gift.create(
+    [
+        {
+            title: 'IDA Pro',
+            description: 'IDA is a Windows, Linux or Mac OS X hosted multi-processor disassembler and debugger that offers so many features it is hard to describe them all. Just grab an evaluation version if you want a test drive.
+
+An executive summary is provided for the non-technical user.',
+            url: 'https://www.hex-rays.com/products/ida/index.shtml',
+            image: 'https://www.hex-rays.com/products/ida/pix/pc1.gif'
+        },
+        {
+            title: 'Cerbero Profiler',
+            description: 'Cerbero Profiler is a tool designed primarily for malware and forensic analysis. It supports a huge number of file formats (listed below) on which it performs analysis and lets the user inspect their internal layout. Profiler is often used to identify 0-day threats and personal information inside of files. However, given the scale of the project, it has many other uses and we encourage you to visit our blog in order to see Profiler in action.',
+            url: 'http://cerbero.io/profiler/',
+            image: 'http://cerbero.io/static/images/profiler/header.png',
+            user: User.find_by_email('user@example.com')
+        },
+        {
+            title: 'Brotschneidemaschine',
+        },
+        {
+            title: 'Spielesammlung',
+            description: 'Eine Spielesammlung mit Mühle, Schach, etc',
+        }
+    ]
+)
