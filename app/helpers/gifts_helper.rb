4a5792342f130a88eef1a59f50affd4db8fc0c82
Dominik Wagner <DomiWag@users.noreply.github.com>
2015-05-28 19:05:26 +0200

    Fix problem related to missing gift description

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -1,11 +1,16 @@
 module GiftsHelper
   SHORT_DESCRIPTION_LENGTH = 80
+  NO_DESCRIPTION_STRING = 'There is no description for this gift, so be creative!'
 
   def shorten(description)
-    if description.length > SHORT_DESCRIPTION_LENGTH then
-      description.first(SHORT_DESCRIPTION_LENGTH) + '...'
+    if description then
+      if description.length > SHORT_DESCRIPTION_LENGTH then
+        description.first(SHORT_DESCRIPTION_LENGTH) + '...'
+      else
+        description
+      end
     else
-      description
+      NO_DESCRIPTION_STRING
     end
   end
 end
