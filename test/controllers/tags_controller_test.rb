15aadcb88604285bed5c2a68f086621a5ebe11cb
Dominik Wagner <DomiWag@users.noreply.github.com>
2015-05-29 15:22:17 +0200

    Remove unnecessary tests

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -11,39 +11,8 @@ class TagsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:tags)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create tag" do
-    assert_difference('Tag.count') do
-      post :create, tag: { name: @tag.name }
-    end
-
-    assert_redirected_to tag_path(assigns(:tag))
-  end
-
   test "should show tag" do
     get :show, id: @tag
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @tag
-    assert_response :success
-  end
-
-  test "should update tag" do
-    patch :update, id: @tag, tag: { name: @tag.name }
-    assert_redirected_to tag_path(assigns(:tag))
-  end
-
-  test "should destroy tag" do
-    assert_difference('Tag.count', -1) do
-      delete :destroy, id: @tag
-    end
-
-    assert_redirected_to tags_path
   end
 end
